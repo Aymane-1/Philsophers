3392950d9243496598a50e92b04a198bcfb2d980
Aeymne Echafii <aechafii@e1r10p15.1337.ma>
2022-09-18 16:04:57 +0100

    Adding input range for number of philos / minimum time to eat, sleep or think, as well as parsing the optional last parameter that consists of defining a limit of turns to eat

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 06:11:32 by aechafii          #+#    #+#             */
-/*   Updated: 2022/09/18 14:12:02 by aechafii         ###   ########.fr       */
+/*   Updated: 2022/09/18 15:55:39 by aechafii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,18 @@
 typedef struct s_table
 {
 	char	**args;
-	int		number_of_philos;
+	int		num_of_philos;
+	int		num_of_forks;
 	int		time_to_die;
 	int		time_to_eat;
 	int		time_to_sleep;
+	int		num_of_iteration_to_eat;
 }	t_table;
 
 int			ft_is_digit(int a);
 long		atoi_philo(char *str);
 void		error_parser(t_table *table);
-void		complete_filling(t_table *table);
+void		test_range(t_table *table);
 void		philo_error(t_table *table);
 t_table		*create_table(t_table *table, char **args);
 
--- philo/src/parser.c
+++ philo/src/parser.c
@@ -6,11 +6,30 @@
 /*   By: aechafii <aechafii@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 11:40:16 by aechafii          #+#    #+#             */
-/*   Updated: 2022/09/18 15:03:04 by aechafii         ###   ########.fr       */
+/*   Updated: 2022/09/18 15:58:22 by aechafii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	test_range(t_table *table)
+{
+	if (atoi_philo(table->args[1]) > 200
+		|| atoi_philo(table->args[2]) < 60
+		|| atoi_philo(table->args[3]) < 60
+		|| atoi_philo(table->args[4]) < 60)
+		philo_error(table);
+	else
+	{	
+		table->num_of_philos = atoi_philo(table->args[1]);
+		table->num_of_forks = atoi_philo(table->args[1]);
+		table->time_to_die = atoi_philo(table->args[2]);
+		table->time_to_eat = atoi_philo(table->args[3]);
+		table->time_to_sleep = atoi_philo(table->args[4]);
+		if (table->args[5])
+			table->num_of_iteration_to_eat = atoi_philo(table->args[5]);
+	}
+}
 
 void	error_parser(t_table *table)
 {
@@ -29,18 +48,14 @@ void	error_parser(t_table *table)
 		while (str[j] != '\0')
 		{
 			argument = atoi_philo(str);
-			if (argument > 2147483647 || argument < -2147483648)
-				philo_error(table);
 			if (str[j] == ' ' || ft_is_digit(str[j])
-				|| (str[j] == '+' && ft_is_digit(str[j + 1])))
+				|| (str[j] == '+' && ft_is_digit(str[j + 1]))
+				|| (argument <= 2147483647 || argument >= -2147483648))
 				j++;
 			else
-			{
 				philo_error(table);
-				exit(-1);
-			}
 		}
 		str = table->args[i++];
 	}
+	test_range(table);
 }
-
